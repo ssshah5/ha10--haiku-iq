a16c343c52a982e50c92906ebc8f769409090c59
Ryan Soley <resoley@us.ibm.com>
2016-08-24 18:03:10 -0400

    add footer styles and info functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
 (function app() {
   'use strict';
 
+  var infoBox = document.getElementById('info-popup');
+  var infoButton = document.getElementById('info');
+  var closeButton = document.getElementById('close');
+
+  infoButton.onclick = function() {
+    infoBox.style.display = 'block';
+  }
+
+  closeButton.onclick = function() {
+    infoBox.style.display = 'none';
+  }
+/*
+  window.onclick = function(e) {
+    if(e.target == info-box) {
+      infoBox.style.display = 'none';
+    }
+  }
+*/
   window.addEventListener('DOMContentLoaded', function appDCL() {
     // Mean to console.log out, so disabling
     console.log('Hello World'); // eslint-disable-line no-console
