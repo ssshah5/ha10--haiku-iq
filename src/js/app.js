7d4cbb0de60025816e17c608645514d3cbad5544
Ryan Soley <resoley@us.ibm.com>
2016-08-24 18:18:55 -0400

    fix linting issues

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,18 @@
 (function app() {
   'use strict';
 
-  var infoBox = document.getElementById('info-popup');
-  var infoButton = document.getElementById('info');
-  var closeButton = document.getElementById('close');
+  var infoBox = document.getElementById('info-popup'),
+    infoButton = document.getElementById('info'),
+    closeButton = document.getElementById('close');
 
-  infoButton.onclick = function() {
+  infoButton.onclick = function () {
     infoBox.style.display = 'block';
-  }
+  };
 
-  closeButton.onclick = function() {
+  closeButton.onclick = function () {
     infoBox.style.display = 'none';
-  }
-/*
-  window.onclick = function(e) {
-    if(e.target == info-box) {
-      infoBox.style.display = 'none';
-    }
-  }
-*/
+  };
+
   window.addEventListener('DOMContentLoaded', function appDCL() {
     // Mean to console.log out, so disabling
     console.log('Hello World'); // eslint-disable-line no-console
