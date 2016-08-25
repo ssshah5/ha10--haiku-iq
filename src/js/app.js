40332fe273c5743cb6ae144e5de4e6c228c29842
Jim McVea <jimmcvea@us.ibm.com>
2016-08-25 19:07:21 -0400

    :art: change input fields for readability

--- src/js/app.js
+++ src/js/app.js
@@ -121,7 +121,7 @@
         button = e.target;
         word = button.innerHTML;
 
-        line = document.getElementById('poemLine' + curLine);
+        line = document.getElementById('poemLineWord' + curLine);
 
         sylAdd = button.getAttribute('data-syl');
         syl = document.getElementById('syl' + curLine);
@@ -133,9 +133,9 @@
         };
 
         lines[curLine - 1].push(wordObj);
-        line.value = '';
+        line.textContent = '';
         for (i = 0; i < lines[curLine - 1].length; i++) {
-          line.value = line.value + lines[curLine - 1][i].word + ' ';
+          line.textContent = line.textContent + lines[curLine - 1][i].word + ' ';
         }
 
         if (curLine === 1 || curLine === 3) {
